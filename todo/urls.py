4c0ae406234e0b6f9bd35000db2d3cf0ed1b77e1
ssobii2 <subhanimran4@gmail.com>
2024-03-28 11:51:19 +0100

    Added CRUD

--- todo/urls.py
+++ todo/urls.py
@@ -5,4 +5,8 @@ from . import views
 
 urlpatterns = [
     path('addTask/', views.addTask, name='addTask'),
+    path('markAsDone/<int:pk>', views.markAsDone, name='markAsDone'),
+    path('markAsNotDone/<int:pk>', views.markAsNotDone, name='markAsNotDone'),
+    path('editTask/<int:pk>', views.editTask, name='editTask'),
+    path('deleteTask/<int:pk>', views.deleteTask, name='deleteTask'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404, redirect, render
 
 from todo.models import Task
 
@@ -6,4 +6,33 @@ from todo.models import Task
 def addTask(request):
     task = request.POST.get('task')
     Task.objects.create(task=task)
+    return redirect('home')
+
+def markAsDone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def markAsNotDone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+
+def editTask(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        task.task = request.POST.get('task')
+        task.save()
+        return redirect('home')
+    else:
+        context = {
+            'task': task
+        }
+        return render(request, 'editTask.html', context)
+    
+def deleteTask(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
     return redirect('home')
